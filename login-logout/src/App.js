37b39754be4294bf5e68d59062f716a28c841dd6
GoAlive13 <95854153+saitej98@users.noreply.github.com>
2022-10-31 11:57:44 +0530

    login and register components added

--- login-logout/src/App.js
+++ login-logout/src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.js";
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import "./App.css";
+import LogIn from "./components/Login/LogIn";
+import Register from "./components/Register/Register";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Router>
+        <Switch>
+          <Route path="/" exact component={LogIn} />
+          <Route path="/register" exact component={Register} />
+        </Switch>
+      </Router>
+      <AboutApp />
     </div>
   );
 }
